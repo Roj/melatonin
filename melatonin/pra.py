c0cb8689ea2a141ee39515d375b62c5c714e33e9
Joaquin Torre <joaquin.torre@muttdata.ai>
2025-01-17 16:24:20 -0300

    Add temporary file for RoomAcoustics simulation

--- melatonin/pra.py
+++ melatonin/pra.py
@@ -14,9 +14,6 @@ logging.basicConfig(
 
 log = logging.getLogger("pra")
 
-# Location of original source
-azimuths = np.deg2rad([30, 120])
-
 
 @dataclass
 class Parameters:
@@ -29,97 +26,133 @@ class Parameters:
     sampling_frequency: int
     nfft: int
     freq_bins: np.ndarray
+    microphone_positions: np.ndarray
 
     @property
     def sigma2(self):
         return 10 ** (-self.SNR / 10) / (4.0 * np.pi * self.distance) ** 2
 
 
+def get_microphone_signals(
+    source_azimuths: np.ndarray, source_signals: np.ndarray, parameters: Parameters
+):
+    """Generating microphone signals by simulating an anechoic room"""
+    aroom = pra.AnechoicRoom(
+        parameters.dim,
+        fs=parameters.sampling_frequency,
+        sigma2_awgn=parameters.sigma2,
+    )
+    aroom.add_microphone_array(
+        pra.MicrophoneArray(parameters.microphone_positions, fs=aroom.fs)
+    )
+    source_locations = (
+        parameters.room_dim / 2
+        + parameters.distance
+        * np.array([np.cos(source_azimuths), np.sin(source_azimuths)]).T
+    )
+    for signal_i in range(parameters.n_sources):
+        location, signal = source_locations[signal_i], source_signals[:, signal_i]
+        aroom.add_source(location, signal=signal)
+
+    aroom.simulate()
+    return aroom.mic_array.signals
+
+
+def run_algorithms(
+    azimuths: list[np.ndarray],
+    source_signals: np.ndarray,
+    parameters: Parameters,
+    algorithms: list[str],
+):
+    results_by_algorithm = {name: dict() for name in algorithms}
+    microphone_signals = get_microphone_signals(azimuths, source_signals, parameters)
+
+    X = np.array(
+        [
+            pra.transform.stft.analysis(signal, parameters.nfft, parameters.nfft // 2).T
+            for signal in microphone_signals
+        ]
+    )
+
+    for algo_name in algorithms:
+        doa = pra.doa.algorithms[algo_name](
+            parameters.microphone_positions,
+            parameters.sampling_frequency,
+            parameters.nfft,
+            c=parameters.speed_of_sound,
+            num_src=len(azimuths),
+        )
+        doa.locate_sources(X, freq_bins=parameters.freq_bins)
+        grnds = np.sort(np.rad2deg(azimuths))
+        preds = np.sort(np.rad2deg(doa.azimuth_recon))
+        try:
+            results_by_algorithm[algo_name]["errors"] = np.linalg.norm(preds - grnds)
+        except ValueError:
+            logging.error(
+                f"Error using algorithm {algo_name}\n"
+                f"Predictions: {preds}\n"
+                f"Grounds: {grnds}\n"
+            )
+            continue
+        results_by_algorithm[algo_name]["grounds"] = grnds
+        results_by_algorithm[algo_name]["predictions"] = preds
+    return results_by_algorithm
+
+
 def run_simulation(
     azimuths_list: list[np.ndarray], source_signals: np.ndarray, parameters: Parameters
 ):
-    # We use a circular array with radius 15 cm # and 12 microphones
-    R = pra.circular_2D_array(parameters.room_dim / 2, 8, 0.0, 0.15)
-    algo_names = sorted(set(pra.doa.algorithms.keys()) - {"FRIDA"})
-    results_by_algorithm = {
-        name: {
-            "errors": [],
-            "grounds": [],
-            "predictions": [],
-        }
-        for name in algo_names
-    }
+    """Run simulation of repeated source detections.
+
+    This method runs the detection algorithms (all except for FRIDA) on the source signals
+    with varying direction of arrivals. The method is intended to help quantify the quality
+    of the predictions.
+
+    Parameters
+    ----------
+    azimuths_list : list[np.ndarray]
+        Sequence of ground DoAs. Each element must be the same length and coincide with the
+        parameters.n_sources.
+    source_signals : list[np.ndarray]
+        The signal of each source. Will be repeated across runs.
+    parameters : Parameters
+        Rest of parameters of the setting (such as room) and algorithms.
+    """
+    algorithms = sorted(set(pra.doa.algorithms.keys()) - {"FRIDA"})
+    results = {algorithm: dict() for algorithm in algorithms}
     for azimuths in azimuths_list:
-        aroom = pra.AnechoicRoom(
-            parameters.dim,
-            fs=parameters.sampling_frequency,
-            sigma2_awgn=parameters.sigma2,
+        partial_results = run_algorithms(
+            azimuths, source_signals, parameters, algorithms
         )
-        aroom.add_microphone_array(pra.MicrophoneArray(R, fs=aroom.fs))
-        source_locations = (
-            parameters.room_dim / 2
-            + parameters.distance * np.array([np.cos(azimuths), np.sin(azimuths)]).T
-        )
-        for signal_i in range(parameters.n_sources):
-            location, signal = source_locations[signal_i], source_signals[:, signal_i]
-            aroom.add_source(location, signal=signal)
+        for algorithm, algorithm_results in partial_results.items():
+            for metric, value in algorithm_results.items():
+                if metric not in results[algorithm]:
+                    results[algorithm][metric] = list()
+                results[algorithm][metric].append(value)
 
-        aroom.simulate()
-
-        X = np.array(
-            [
-                pra.transform.stft.analysis(
-                    signal, parameters.nfft, parameters.nfft // 2
-                ).T
-                for signal in aroom.mic_array.signals
-            ]
-        )
-
-        for algo_name in algo_names:
-            doa = pra.doa.algorithms[algo_name](
-                R,
-                parameters.sampling_frequency,
-                parameters.nfft,
-                c=parameters.speed_of_sound,
-                num_src=len(azimuths),
-            )
-            doa.locate_sources(X, freq_bins=parameters.freq_bins)
-            grnds = np.sort(np.rad2deg(doa.azimuth_recon))
-            preds = np.sort(np.rad2deg(azimuths))
-            try:
-                results_by_algorithm[algo_name]["errors"].append(
-                    np.linalg.norm(preds - grnds)
-                )
-            except ValueError:
-                logging.error(
-                    f"Error using algorithm {algo_name}\n"
-                    f"Predictions: {preds}\n"
-                    f"Grounds: {grnds}\n"
-                )
-                continue
-            results_by_algorithm[algo_name]["grounds"].append(grnds)
-            results_by_algorithm[algo_name]["predictions"].append(preds)
-    return results_by_algorithm
+    return results
 
 
 if __name__ == "__main__":
     from rich.pretty import pprint
 
+    room_dim = np.r_[10.0, 10.0]
     parameters = Parameters(
         n_sources=3,
         distance=3.0,
         dim=2,
-        room_dim=np.r_[10.0, 10.0],
+        room_dim=room_dim,
         SNR=0.0,
         speed_of_sound=343.0,
-        sampling_frequency=16000,
+        sampling_frequency=16_000,
         nfft=256,
         freq_bins=np.arange(5, 60),
+        # We use a circular array with radius 15 cm # and 12 microphones
+        microphone_positions=pra.circular_2D_array(room_dim / 2, 12, 0.0, 0.15),
     )
     t = np.linspace(0, 1, num=int(parameters.sampling_frequency * 1))
     source_signals = np.array(
         [
-            # (np.sin(t*j*400*2*np.pi)).astype(np.float32)
             (scipy.signal.sawtooth(t * j * 400 * 2 * np.pi)).astype(np.float32)
             for j in range(1, parameters.n_sources + 1)
         ]
